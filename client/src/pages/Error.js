3317bb4679ffa98050c7a537649e2f2a8d427949
Justin Ho <hochungyan@live.hk>
2022-12-19 00:10:19 +0000

    Error Page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,17 @@
+import {Link} from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 const Error = () =>
 {
     return (
-<h1>Error</h1>
-
+<Wrapper className='full-page'>
+    <div>
+        <img src ={img} alt ='not found' />
+        <h3>ohh! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+    <Link to ='/'>Back to Home Page</Link>
+    </div>
+</Wrapper>
 
     )
 }
